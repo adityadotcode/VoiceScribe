53e4a1ff9f124033e9db6d57e19620a176adf3b1
Aditya_Shukla <masteradityashukla@gmail.com>
2026-09-19 16:29:35 +0530

    fixed db connection error

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
 const app = require('./app');
 const { port, mongodbUri } = require('./config/env');
 const { connectDb } = require('./config/db');
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 app.listen(port, () => {
   console.log(`VoiceScribe server listening on port ${port}`);
